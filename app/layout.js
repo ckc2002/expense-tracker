380d093f26d7a4959accab88cb7704cd13dc2c5c
Chaitanya Kumar Chandra <chaitanyachandra2000@gmail.com>
2024-06-19 23:54:00 +0530

    favicon-change

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter, Outfit } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
+import Head from "next/head";
 
 const outfit = Outfit({ subsets: ["latin"] });
 // const inter = Inter({ subsets: ["latin"] });
@@ -14,6 +15,10 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
+        <Head>
+          <title>{metadata.title}</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
         <Toaster />
         <body className={outfit.className}>{children}</body>
       </html>
