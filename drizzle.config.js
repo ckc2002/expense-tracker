9f9e441e970db96a5e5408604175cdbd31f1051f
Chaitanya Kumar Chandra <chaitanyachandra2000@gmail.com>
2024-06-26 11:17:03 +0530

    fix-env

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.jsx",
     dialect: 'postgresql',
     dbCredentials: {
-        url: "postgresql://expenseMoney_owner:sYN7KdhDeW9H@ep-empty-hat-a5ha4hl1.us-east-2.aws.neon.tech/Expense-Tracker?sslmode=require",
+        url: process.env.NEXT_PUBLIC_DATABASE_URL,
     }
 } 
--- utils/dbConfig.jsx
+++ utils/dbConfig.jsx
@@ -1,5 +1,5 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import { schema } from './schema';
-const sql = neon("postgresql://expenseMoney_owner:sYN7KdhDeW9H@ep-empty-hat-a5ha4hl1.us-east-2.aws.neon.tech/Expense-Tracker?sslmode=require");
+const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL);
 export const db = drizzle(sql, { schema })
